dfe200fffa265f0f0dbd7bc6d1cde2dbaf68d17e
SamoKopecky <sano.kopecky@gmail.com>
2019-10-18 06:34:53 +0200

    commited to revert

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from Cell import *
 from Gui import *
-import re
 import sys
 import os
 import random
+
+
+def cords_from_file(file_name="cords.txt"):
+    coords = []
+    file = open(file_name, "r")
+    for line in file.readlines():
+        coords.append(line)
+    file.close()
+    return coords
+
 
 def clear_screen():
     os.system('clear')
@@ -12,21 +21,6 @@ def clear_screen():
 def print_there(x, y, text):
     sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
     sys.stdout.flush()
-
-
-def create_first_list():
-    print("Enter positions:")
-    pattern = r'(\d+),(\d+)'
-    cords = []
-    while True:
-        input_cords = input()
-        if not re.match(pattern, input_cords):
-            return cords
-
-        middle_index = input_cords.index(',')
-        first_num = int(input_cords[0:middle_index])
-        second_num = int(input_cords[middle_index + 1:])
-        cords.append(Cell(first_num, second_num, True))
 
 
 def print_cells(positions):
@@ -44,7 +38,6 @@ def init_all_cells(alive_cells):
 
 
 def init_game():
-    # positions = create_first_list()
     positions = [Cell(1, 1), Cell(2, 2), Cell(3, 1), Cell(3, 2), Cell(3, 0),
                  Cell(6, 6), Cell(7, 7), Cell(8, 6), Cell(8, 7), Cell(8, 5)]
     print_cells(positions)
